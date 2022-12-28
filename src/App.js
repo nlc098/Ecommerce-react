523bbe1eae61b280eb35bc8381f32cb902fa8c6d
Nicolás Lepore Banegas <nicolepore2@gmail.com>
2022-12-28 19:22:46 -0300

    prueba rama 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+//componentes 
+import Comp1 from './components/Comp1'
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <Comp1 />
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
--- src/components/Comp1.jsx
+++ src/components/Comp1.jsx
@@ -3,7 +3,7 @@ import react from "react"
 const Comp1 = () =>{
     return(
         <>
-        
+            <h1>hola mundo</h1>
         
         </>
     ) 
